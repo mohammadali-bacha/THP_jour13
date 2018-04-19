c8450aad38c07a69ac51c8df20b536069b321dc4
MOHAMMAD-ALI <bacha@tuta.io>
2018-04-19 15:33:13 +0200

    2nd commit

--- morpion.rb
+++ morpion.rb
@@ -1,13 +1,19 @@
+require 'binding.pry'
 class BoardCase
   #TO DO : la classe a 2 attr_accessor, sa valeur (X, O, ou vide), ainsi que son numéro de case)
 
-
   def initialize
     #TO DO doit régler sa valeur, ainsi que son numéro de case
+    attr_accessor :value, :case_number
+    BoardCase.case_number =
+    BoardCase.value = "x" "O" " "
+    # :x , :0 , :" "
   end
+
 
   def to_s
     #TO DO : doit renvoyer la valeur au format string
+  #  attr_accessor :case_number 2eme attr_accessor ou bien c'est ds la premiere methode ?
   end
 
 end
@@ -15,48 +21,58 @@ end
 class Board
   include Enumerable
   #TO DO : la classe a 1 attr_accessor, une array qui contient les BoardCases
+  attr_accessor :BoardCases[]
 
+def initialize
+  #TO DO :
+  #Quand la classe s'initialize, elle doit créer 9 instances BoardCases
+  #Board.BoardCase
 
-  def initialize
-    #TO DO :
-    #Quand la classe s'initialize, elle doit créer 9 instances BoardCases
-    #Ces instances sont rangées dans une array qui est l'attr_accessor de la classe
-  end
+  #Ces instances sont rangées dans une array qui est l'attr_accessor de la classe
+  attr_accessor :array
+end
 
-  def to_s
+def to_s
   #TO DO : afficher le plateau
+  p Board
+end
 
-  end
+def play
+  #TO DO : une méthode qui change la BoardCase jouée en fonction de la valeur du joueur (X, ou O)
+end
 
-  def play
-    #TO DO : une méthode qui change la BoardCase jouée en fonction de la valeur du joueur (X, ou O)
-  end
-
-  def victory?
-    #TO DO : qui gagne ?
-  end
+def victory?
+  #TO DO : qui gagne ?
+end
 
 class Player
   #TO DO : la classe a 2 attr_accessor, son nom, sa valeur (X ou O). Elle a un attr_writer : il a gagné ?
-
+  attr_accessor :name , :value
 
   def initialize
     #TO DO : doit régler son nom, sa valeur, son état de victoire
   end
 
-class Game
-  def initialize
-    #TO DO : créé 2 joueurs, créé un board
+  class Game
+    def initialize
+      #TO DO : créé 2 joueurs, créé un board
+      Player1 = Player.new
+      Player2 = Player.new
+      new_Board = Board.new
+      #    initialise 9 instances de BoardCases).
+      # Game s'occupe de finir la partie si un joueur a gagné, de demander aux utilisateurs où ils veulent jouer
+      # à chaque tour
+    end
+
+    def go
+      # TO DO : lance la partie
+    end
+
+    def turn
+      #TO DO : affiche le plateau, demande au joueur il joue quoi, vérifie si un joueur a gagné,
+      #passe au joueur suivant si la partie n'est pas finie
+    end
+
   end
 
-  def go
-    # TO DO : lance la partie
-  end
-
-  def turn
-    #TO DO : affiche le plateau, demande au joueur il joue quoi, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie
-  end
-
-end
-
-Game.new.go
+  Game.new.go
